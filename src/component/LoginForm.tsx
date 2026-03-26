50afccd1dca929635fb0f414a582b4f162dd941c
Laska254 <w.turczak254@gmail.com>
2026-03-26 21:28:38 +0100

    Most basic login form

---
+++ src/component/LoginForm.tsx
@@ -0,0 +1,47 @@
+import {useState} from "react";
+import {Button, TextField} from "@mui/material";
+
+function LoginForm() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit() {
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="email">
+                    Email
+                </label>
+                <TextField
+                    id="email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="your@email.com"
+                />
+            </div>
+            <div>
+                <label htmlFor="password">
+                    Password
+                </label>
+                <TextField
+                    id="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="password"
+                />
+            </div>
+            <Button
+                variant="contained"
+                type="submit"
+            />
+        </form>
+    );
+
+}
+
+export default LoginForm;
