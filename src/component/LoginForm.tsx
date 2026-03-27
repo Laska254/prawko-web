059fc513eb52e1dc9c121d2138469bc46432fe39
Laska254 <w.turczak254@gmail.com>
2026-03-27 13:07:06 +0100

    Or like that

--- src/component/LoginForm.tsx
+++ src/component/LoginForm.tsx
@@ -2,37 +2,34 @@ import {useState} from "react";
 import {Button, TextField} from "@mui/material";
 import axios from "axios";
 
-interface LoginDto {
-    email: string;
-    password: string;
-}
 
 function LoginForm() {
 
-    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    function login() {
-        const dto: LoginDto = {
-            email,
-            password
-        }
-        axios.post("https://localhost:8080/auth/login", dto, {headers: {"Content-Type": "application/json"}});
+    async function login(e: React.FormEvent) {
+        e.preventDefault()
+        await axios.post("https://localhost:8080/auth/login", document.querySelector('#loginForm'), {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
     }
 
     return (
         <div>
-            <form onSubmit={login}>
+            <form id={"loginForm"} onSubmit={login}>
                 <div>
-                    <label htmlFor="email">
-                        Email
+                    <label htmlFor="username">
+                        Username
                     </label>
                     <TextField
-                        id="email"
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="your@email.com"
+                        id="username"
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="username"
                     />
                 </div>
                 <div>
